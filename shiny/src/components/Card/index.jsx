1f0394c2b150be1836e08047b8ff11b79579ab46
soaliyek <soaliye.kindo@ashesi.edu.gh>
2026-05-21 00:54:44 +0100

    Customizing Card Style using styled-components

--- shiny/src/components/Card/index.jsx
+++ shiny/src/components/Card/index.jsx
@@ -1,20 +1,39 @@
 import PropTypes from 'prop-types';
 import DefaultPicture from '../../assets/profile.png';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  gap: 10px;
+  background-color: #ae1616;
+`;
+
+const CardLabel = styled.span`
+  font-size: 22px;
+  color: #5843e4;
+  font-weight: bold;
+`;
+
+const CardTitle = styled.span`
+  font-size: 18px;
+  color: #fff;
+`;
+
+const CardImage = styled.img`
+height: 80px;
+width: 80px;
+border-radius: 50%;
+`
 
 function Card({ label, title, picture }) {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 15,
-        gap: 10,
-      }}
-    >
-      <span> {label} </span>
-      <img src={picture} alt="freelancer" height="80px" width="80px" />
-      <span> {title}</span>
-    </div>
+    <CardContainer>
+      <CardLabel> {label} </CardLabel>
+      <CardImage src={picture} alt="freelancer" />
+      <CardTitle> {title} </CardTitle>
+    </CardContainer>
   );
 }
 
